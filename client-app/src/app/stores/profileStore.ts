678fbe21ae48e5992fd17e4b26e030aceaf91cf9
Carlos Munoz <42192364+codewithcarlos@users.noreply.github.com>
2019-09-19 16:11:32 -0700

    Added paging and filtering S21

--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -1,6 +1,6 @@
 import { RootStore } from './rootStore';
 import { observable, runInAction, action, computed, reaction } from 'mobx';
-import { IProfile, IPhoto } from '../models/profile';
+import { IProfile, IPhoto, IUserActivity } from '../models/profile';
 import agent from '../api/agent';
 import { toast } from 'react-toastify';
 
@@ -19,7 +19,7 @@ export default class ProfileStore {
           this.followings = [];
         }
       }
-    )
+    );
   }
 
   @observable profile: IProfile | null = null;
@@ -28,6 +28,8 @@ export default class ProfileStore {
   @observable loading = false;
   @observable followings: IProfile[] = [];
   @observable activeTab: number = 0;
+  @observable userActivities: IUserActivity[] = [];
+  @observable loadingActivities = false;
 
   @computed get isCurrentUser() {
     if (this.rootStore.userStore.user && this.profile) {
@@ -37,9 +39,28 @@ export default class ProfileStore {
     }
   }
 
+  @action loadUserActivities = async (username: string, predicate?: string) => {
+    this.loadingActivities = true;
+    try {
+      const activities = await agent.Profiles.listActivities(
+        username,
+        predicate!
+      );
+      runInAction(() => {
+        this.userActivities = activities;
+        this.loadingActivities = false;
+      });
+    } catch (error) {
+      toast.error('Problem loading activities');
+      runInAction(() => {
+        this.loadingActivities = false;
+      });
+    }
+  };
+
   @action setActiveTab = (activeIndex: number) => {
     this.activeTab = activeIndex;
-  }
+  };
 
   @action loadProfile = async (username: string) => {
     this.loadingProfile = true;
